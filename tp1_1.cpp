856626c68544fef817e99da0ccc4e7fa5b08e26b
“Matias” <mattmedi08@gmail.com>
2024-03-26 13:42:08 -0300

    cuarto commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,6 +3,20 @@
 
 int main(){
 
+    int numero = 3, *pnumero;
+
+    pnumero = &numero;
+
+    printf("\n\nEl puntero contiene: %d ",*pnumero);
+
+    printf("\n\nLa Direccion de memoria que guarda el puntero es: %p ",pnumero);
+
+    printf("\n\nLa Direccion de memoria de la variable es: %p ",&numero);
+
+    printf("\n\nLa Direccion de memoria del puntero es: %p ",&pnumero);
+
+    printf("\n\nEl tamaño de memoria que usa la variable es de: %d bytes ", sizeof(numero));
+
     printf("Hola Mundo");
 
     return 0;
